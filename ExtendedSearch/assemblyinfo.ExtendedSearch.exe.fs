2baa1392b09587da700dc7287d8271ba0235a38c
unknown <Onorio_Development@.(none)>
2010-03-10 21:20:57 -0500

    Added new details for the assembly

---
+++ ExtendedSearch/assemblyinfo.ExtendedSearch.exe.fs
@@ -0,0 +1,11 @@
+﻿namespace ExtendedSearch
+
+    open System.Reflection
+    
+    [<assembly:AssemblyDescription("ExtendedSearch.exe")>]
+    [<assembly:AssemblyCompany("Onorio Catenacci")>]
+    [<assembly:AssemblyTitle("ExtendedSearch.exe")>]
+    [<assembly:AssemblyProduct("ExtendedSearch.exe")>]
+    [<assembly:System.Runtime.InteropServices.ComVisibleAttribute(false)>]
+    [<assembly:System.CLSCompliantAttribute(true)>]
+    do()
