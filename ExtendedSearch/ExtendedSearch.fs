e251372caec0dd9d2735be8c9250ffaa633097ac
unknown <Onorio_Development@.(none)>
2010-03-10 21:24:32 -0500

    Added assembly info for exe and modified project to add new assembly info and change the build order.  Also modified the main code to build the dialog slightly differently

--- ExtendedSearch/ExtendedSearch.fs
+++ ExtendedSearch/ExtendedSearch.fs
@@ -1,7 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.net
 
 namespace ExtendedSearch
+
+    open System.Windows.Forms
     module UI =  
         [<EntryPoint>]
         let main(args:string[]) =
+            let esMainForm = new Form(Text="ExtendedSearch",TopMost=true, Height=500, Width=500)
+            let nameLabel = new Label(Text="Name",Width=100,Left=10,Top=10)
+            let nameButton = new Button()
+            esMainForm.Controls.Add(nameLabel)
+            esMainForm.Controls.Add(nameButton)
+            Application.Run(esMainForm)
             0
--- ExtendedSearch/assemblyinfo.ExtendedSearch.exe.fs
+++ ExtendedSearch/assemblyinfo.ExtendedSearch.exe.fs
@@ -6,6 +6,7 @@
     [<assembly:AssemblyCompany("Onorio Catenacci")>]
     [<assembly:AssemblyTitle("ExtendedSearch.exe")>]
     [<assembly:AssemblyProduct("ExtendedSearch.exe")>]
+    [<assembly:AssemblyVersion("0.0.1")>]
     [<assembly:System.Runtime.InteropServices.ComVisibleAttribute(false)>]
     [<assembly:System.CLSCompliantAttribute(true)>]
     do()
