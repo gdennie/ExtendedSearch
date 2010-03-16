69d85a1b449d0e051f97db051914a5f8b1ea8794
unknown <Onorio_Development@.(none)>
2010-03-15 21:34:23 -0400

    Adding resources to the project

--- ExtendedSearch/ExtendedSearch.fs
+++ ExtendedSearch/ExtendedSearch.fs
@@ -1,13 +1,23 @@
 ﻿namespace ExtendedSearch
 
     open System.Windows.Forms
+    open System.Resources
+    open System.Reflection
+    
     module UI =  
         [<EntryPoint>]
         let main(args:string[]) =
             let esMainForm = new Form(Text="ExtendedSearch",TopMost=true, Height=500, Width=500)
             let nameLabel = new Label(Text="Name",Width=100,Left=10,Top=10)
-            let nameButton = new Button()
-            esMainForm.Controls.Add(nameLabel)
-            esMainForm.Controls.Add(nameButton)
+            let esRM = new ResourceManager("ExtendedSearch",Assembly.GetExecutingAssembly())
+            
+            let searchButtonCaption = esRM.GetString("SearchButtonCaption")
+            let searchButton = new Button(Text=searchButtonCaption)
+            let cancelButtonCaption = esRM.GetString("CancelButtonCaption")
+            let cancelButton = new Button(Text=cancelButtonCaption)
+//            let buttons = [|searchButton;cancelButton|]
+//            esMainForm.Controls.Add(nameLabel)
+            esMainForm.Controls.Add(searchButton)
+            esMainForm.Controls.Add(cancelButton)
             Application.Run(esMainForm)
             0
